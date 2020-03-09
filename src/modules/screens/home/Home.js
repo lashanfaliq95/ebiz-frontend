a92ee2d28630e5ca5c12f2e87d99add4c9cc07de
lfal7838 <lashan.faliq@syscolabs.com>
2020-03-09 09:24:06 +0530

    Implement api for home

--- src/modules/screens/home/Home.js
+++ src/modules/screens/home/Home.js
@@ -1,66 +1,147 @@
-import React, { useState } from 'react';
-import { Formik, Form } from 'formik';
+import React, { useState } from "react";
+import { Formik, Form } from "formik";
 
-import { AppInput, AppButton } from '../../components';
+import { AppInput, AppButton } from "../../components";
 
-import styles from './Home.module.css';
+import styles from "./Home.module.css";
 
 const Home = props => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const onClickSubmit = values => {
+    setLoading(true);
+    console.log("clicked", values);
+    fetch("http://localhost:8080/api/", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(values)
+    })
+      .then(res => {
+        setLoading(false);
+      })
+      .catch(err => {
+        setLoading(false);
+        setError(err.toString());
+      });
+  };
   return (
     <div className={styles.root__div}>
       <Formik
         initialValues={{
-          firstName: '',
-          lastName: '',
-          companyName: '',
-          number: '',
-          city: '',
-          address: '',
-          services: '',
-          phoneNumber: '',
-          whatsappNumber: '',
-          emailAddress: '',
-          googleLocation: '',
-          facebookLink: '',
-          instagramLink: '',
-          companyWebsite: '',
-          imageUrl: ''
+          firstName: "",
+          lastName: "",
+          companyName: "",
+          number: "",
+          city: "",
+          address: "",
+          services: "",
+          phoneNumber: "",
+          whatsappNumber: "",
+          emailAddress: "",
+          googleLocation: "",
+          facebookLink: "",
+          instagramLink: "",
+          companyWebsite: "",
+          imageUrl: ""
         }}
+        onSubmit={onClickSubmit}
       >
         {({ values }) => {
           return (
             <Form className={styles.form__div}>
-              <label className={styles.form__header}>Generate Digital Business Card</label>
+              <label className={styles.form__header}>
+                Generate Digital Business Card
+              </label>
               <div className={styles.form__container}>
                 <div className={styles.flex__div}>
-                  <label className={styles.section__header}>Mandatory Fields</label>
+                  <label className={styles.section__header}>
+                    Mandatory Fields
+                  </label>
                   <div className={styles.nested__div}>
-                    <AppInput name="firstName" type="text" placeholder="Enter First Name" loading={loading} />
+                    <AppInput
+                      name="firstName"
+                      type="text"
+                      placeholder="Enter First Name"
+                      loading={loading}
+                    />
                     <div className={styles.separator__div} />
-                    <AppInput name="lastName" type="text" placeholder="Enter Last Name" loading={loading} />
+                    <AppInput
+                      name="lastName"
+                      type="text"
+                      placeholder="Enter Last Name"
+                      loading={loading}
+                    />
                   </div>
-                  <AppInput name="companyName" type="text" placeholder="Enter Company Name" loading={loading} />
+                  <AppInput
+                    name="companyName"
+                    type="text"
+                    placeholder="Enter Company Name"
+                    loading={loading}
+                  />
                   <div className={styles.nested__div}>
-                    <AppInput name="number" type="text" placeholder="Enter Phone Number" loading={loading} />
+                    <AppInput
+                      name="number"
+                      type="text"
+                      placeholder="Enter Phone Number"
+                      loading={loading}
+                    />
                     <div className={styles.separator__div} />
-                    <AppInput name="city" type="text" placeholder="Enter City" loading={loading} />
+                    <AppInput
+                      name="city"
+                      type="text"
+                      placeholder="Enter City"
+                      loading={loading}
+                    />
                   </div>
-                  <AppInput name="address" type="text" placeholder="Enter Address" loading={loading} />
-                  <AppInput name="services" type="text" placeholder="Enter Services" loading={loading} />
-                  <AppInput name="emailAddress" type="email" placeholder="Enter Email Address" loading={loading} />
+                  <AppInput
+                    name="address"
+                    type="text"
+                    placeholder="Enter Address"
+                    loading={loading}
+                  />
+                  <AppInput
+                    name="services"
+                    type="text"
+                    placeholder="Enter Services"
+                    loading={loading}
+                  />
+                  <AppInput
+                    name="emailAddress"
+                    type="email"
+                    placeholder="Enter Email Address"
+                    loading={loading}
+                  />
                 </div>
                 <div className={styles.div__separator} />
                 <div className={styles.flex__div}>
-                  <label className={styles.section__header}>Optional Fields</label>
+                  <label className={styles.section__header}>
+                    Optional Fields
+                  </label>
                   <div className={styles.nested__div}>
-                    <AppInput name="phoneNumber" type="text" placeholder="Enter Phone Number" loading={loading} />
+                    <AppInput
+                      name="phoneNumber"
+                      type="text"
+                      placeholder="Enter Phone Number"
+                      loading={loading}
+                    />
                     <div className={styles.separator__div} />
-                    <AppInput name="whatsappNumber" type="text" placeholder="Enter WhatsApp Number" loading={loading} />
+                    <AppInput
+                      name="whatsappNumber"
+                      type="text"
+                      placeholder="Enter WhatsApp Number"
+                      loading={loading}
+                    />
                   </div>
-                  <AppInput name="googleLocation" type="text" placeholder="Enter Location" loading={loading} />
+                  <AppInput
+                    name="googleLocation"
+                    type="text"
+                    placeholder="Enter Location"
+                    loading={loading}
+                  />
                   <AppInput
                     name="facebookLink"
                     type="text"
@@ -73,15 +154,25 @@ const Home = props => {
                     placeholder="Enter Instagram Profile URL"
                     loading={loading}
                   />
-                  <AppInput name="companyWebsite" type="text" placeholder="Enter Company Website" loading={loading} />
-                  <AppInput name="imageUrl" type="text" placeholder="Enter Image URL" loading={loading} />
+                  <AppInput
+                    name="companyWebsite"
+                    type="text"
+                    placeholder="Enter Company Website"
+                    loading={loading}
+                  />
+                  <AppInput
+                    name="imageUrl"
+                    type="text"
+                    placeholder="Enter Image URL"
+                    loading={loading}
+                  />
                 </div>
               </div>
               <AppButton
                 text="Generate Business Card"
                 type="submit"
                 loading={loading}
-                containerStyle={{ marginTop: '20px' }}
+                containerStyle={{ marginTop: "20px" }}
               />
               {error && <label className={styles.main__error}>{error}</label>}
             </Form>
